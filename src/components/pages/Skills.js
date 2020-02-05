65da59a6e416ff73dc70a4ca5562fcfb9abf301a
garyhuangdev <jiazhengsang@gmail.com>
2020-02-05 15:03:58 +1100

    :pencil: update skills section

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -12,28 +12,26 @@ const Skills = () => (
         <h3>Tech Skills</h3>
         <p>
           • Front-End Development: React.js, Redux, Redux-Saga, CSS3, Sass,
-          Less, HTML5, Pug(Jade), JavaScript, AJAX, React Context API, React
-          Hooks, Ant.design, Bootstrap, Materialize, Semantic UI, Responsive
-          design, Webpack, ES6
+          Less, HTML5, Pug(Jade), JavaScript(ES6+), AJAX, React Context API, React
+          Hooks, Ant.design, Bootstrap, Material UI, styled-components, Responsive
+          design, Webpack
         </p>
         <p>
           • Back-End Development: Node.js, ExpressJS, Mongoose, RESTful API,
-          KeystoneJS, PHP, CodeIgniter
+          KeystoneJS, PHP
         </p>
-        <p>• Database design and management: MySQL, MongoDB</p>
+        <p>• Database design and management: MySQL, MongoDB, PostgreSQL</p>
         <p>
-          • Coding Languages: CSS3, HTML5, JavaScript, Python, SQL, PHP, MATLAB
+          • Coding Languages: CSS3, HTML5, JavaScript, Python, SQL, PHP
         </p>
-        <p>• Tool: VSCode, Visual Studio, Eclipse, Jupyter Notebook</p>
         <p>• Cloud: AWS S3, EC2, Heroku, MongoDB Atlas</p>
         <p>
-          • Testing: TAM, SUS, Time on Task, A/B testing, Cognitive walkthrough,
-          Heuristic evaluation
+          • Testing: Jest, Enzyme, Mocha
         </p>
         {/* Teamwork Skills */}
         <h3>Teamwork Skills</h3>
-        <p>• Tool: Git, Github, Trello, Bitbucket, Jira</p>
-        <p>• Methodology: Agile, Scrum</p>
+        <p>• Tool: Git, AWS, NPM, Webpack, Bitbucket, JIRA, Postman, ESLint, Prettier, Autoprefixer</p>
+        <p>• Methodology: BDD, TDD, CI/CD, Agile/Scrum</p>
       </div>
     </div>
   </section>
