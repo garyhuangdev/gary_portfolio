6de0388f7d75133a727cce3599da0aa4f34de196
garyhuangdev <jiazhengsang@gmail.com>
2020-02-13 00:07:50 +1100

    update resume

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import resume from '../../resume/Gary-Huang_Developer_Resume.pdf';
+import resume from '../../resume/Gary Huang_Full Stack Developer.pdf';
 
 const Home = () => (
   <section className="home">
